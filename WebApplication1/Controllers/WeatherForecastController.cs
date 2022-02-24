2b349c03a1f0793e6b095aa25df110570791518c
trexa81 <trex81@yandex.ru>
2022-02-24 13:23:31 +0300

    new

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace WebApplication1.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                Temperature_C = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
--- WebApplication1/WeatherForecast.cs
+++ WebApplication1/WeatherForecast.cs
@@ -6,9 +6,9 @@ namespace WebApplication1
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int Temperature_C { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(Temperature_C / 0.5556);
 
         public string Summary { get; set; }
     }
